9fc06759db18a4b6f706dea4b68939fa63e38b5c
Gabriel Patrício <gabrieltp087@gmail.com>
2023-03-01 23:00:38 -0300

    Finishing the app

---
+++ src/routes.js
@@ -0,0 +1,78 @@
+import { Database } from './database.js'
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const users = database.select('users', search ? {
+        name: search,
+        email: search,
+      } : null)
+
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
+      const { name, email } = req.body
+
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    }
+
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      const user = database.select('users', {
+        id,
+      })
+
+      if (!user) {
+        return res.writeHead(404).end()
+      }
+
+      database.update('users', id, { name, email })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const user = database.select('users', {
+        id,
+      })
+
+      if (!user) {
+        return res.writeHead(404).end()
+      }
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  }
+]
--- src/server.js
+++ src/server.js
@@ -1,27 +1,26 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
-// stateful -> salva em memoria enquanto nao parar a aplicação
-// stateless -> não salva nada em memoria, salva em bds
-
-const users = []
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
-  }
+  await json(req, res)
 
-  if (method === 'POST' && url === '/users') {
-    users.push({
-      id: 1,
-      name: 'Biel',
-      email: 'biel@gmail.com'
-    })
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res.writeHead(201).end()
+  if(route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params} = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
